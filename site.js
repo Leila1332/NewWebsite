d4a78d2409db8618079d8e2bb18a0a0c07308ab8
Leila1332 <112931748+Leila1332@users.noreply.github.com>
2022-10-05 12:55:50 +0200

    Add files via upload

---
+++ site.js
@@ -0,0 +1,21 @@
+function WelcomeMessage() {
+    alert("****welcome  to my site****"):
+}
+
+function GotoPage5() {
+    var flag = confirm("Arue you sure to continue");
+    if (flag == true) window.location.href = "page05.html";
+
+}
+
+function GotoPage6() {
+    var flag = confirm("Arue you sure to continue");
+    if (flag == true) window.location.href = "page06.html";
+
+}
+
+function GotoPage7() {
+    var flag = confirm("Arue you sure to continue");
+    if (flag == true) window.location.href = "page07.html";
+
+}
